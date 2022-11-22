356e9b3fc9cc96cd3169ad6dd321875eb26e9a78
Ben Botsford <ben.botsford@flatironschool.com>
2022-11-22 08:50:40 -0700

    re-add debug.py

---
+++ many_to_many/debug.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Game, Review # User
+
+if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///many_to_many.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    import ipdb; ipdb.set_trace()
