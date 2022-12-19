b515ba63701a789d557582ea6c52327ca4a2ea04
Ben Botsford <ben.botsford@flatironschool.com>
2022-12-19 11:30:02 -0600

    fixes #5

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy import create_engine, func
+from sqlalchemy import ForeignKey, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
