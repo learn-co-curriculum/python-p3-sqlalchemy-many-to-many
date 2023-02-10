2d2dc8e3e2e8df8221727ecf7498f59861c57e4c
Ben Botsford <ben.botsford@flatironschool.com>
2023-02-10 14:27:07 -0600

    add db to version control

---
+++ lib/migrations/versions/56e1703a07b1_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: 56e1703a07b1
+Revises: 
+Create Date: 2023-02-10 14:22:12.184497
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56e1703a07b1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -82,89 +82,6 @@ Base = declarative_base()
 # Association object
 # PASSING
 
-class Game(Base):
-    __tablename__ = 'games'
-
-    id = Column(Integer(), primary_key=True)
-    title = Column(String())
-    genre = Column(String())
-    platform = Column(String())
-    price = Column(Integer())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
-
-    reviews = relationship('Review', backref=backref('game'))
-    game_users = relationship('GameUser', back_populates='game')
-    users = association_proxy('game_users', 'user',
-        creator=lambda us: GameUser(user=us))
-
-    def __repr__(self):
-
-        return f'Game(id={self.id}, ' + \
-            f'title={self.title}, ' + \
-            f'platform={self.platform})'
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
-
-    reviews = relationship('Review', backref=backref('user'))
-    game_users = relationship('GameUser', back_populates='user')
-    games = association_proxy('game_users', 'game',
-        creator=lambda gm: GameUser(game=gm))
-
-    def __repr__(self):
-
-        return f'User(id={self.id}, ' + \
-            f'name={self.name})'
-
-class Review(Base):
-    __tablename__ = 'reviews'
-
-    id = Column(Integer(), primary_key=True)
-
-    score = Column(Integer())
-    comment = Column(String())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
-
-    game_id = Column(Integer(), ForeignKey('games.id'))
-    user_id = Column(Integer(), ForeignKey('users.id'))
-
-    def __repr__(self):
-
-        return f'Review(id={self.id}, ' + \
-            f'score={self.score}, ' + \
-            f'game_id={self.game_id})'
-
-class GameUser(Base):
-    __tablename__ = "game_users"
-
-    id = Column(Integer(), primary_key=True)
-    game_id = Column(ForeignKey('games.id'))
-    user_id = Column(ForeignKey('users.id'))
-
-    game = relationship('Game', back_populates='game_users')
-    user = relationship('User', back_populates='game_users')
-
-    def __init__(self, game=None, user=None):
-        self.game = game
-        self.user = user
-
-    def __repr__(self):
-
-        return f'GameUser(game_id={self.game_id}, ' + \
-            f'user_id={self.user_id})'
-
-#######################################
-
-# Association object model
-# PASSING
-
 # class Game(Base):
 #     __tablename__ = 'games'
 
@@ -176,9 +93,10 @@ class GameUser(Base):
 #     created_at = Column(DateTime(), server_default=func.now())
 #     updated_at = Column(DateTime(), onupdate=func.now())
 
-#     reviews = relationship('Review', back_populates='game')
-#     users = association_proxy('reviews', 'user',
-#         creator=lambda us: Review(user=us))
+#     reviews = relationship('Review', backref=backref('game'))
+#     game_users = relationship('GameUser', back_populates='game')
+#     users = association_proxy('game_users', 'user',
+#         creator=lambda us: GameUser(user=us))
 
 #     def __repr__(self):
 
@@ -194,9 +112,10 @@ class GameUser(Base):
 #     created_at = Column(DateTime(), server_default=func.now())
 #     updated_at = Column(DateTime(), onupdate=func.now())
 
-#     reviews = relationship('Review', back_populates='user')
-#     games = association_proxy('reviews', 'game',
-#         creator=lambda gm: Review(game=gm))
+#     reviews = relationship('Review', backref=backref('user'))
+#     game_users = relationship('GameUser', back_populates='user')
+#     games = association_proxy('game_users', 'game',
+#         creator=lambda gm: GameUser(game=gm))
 
 #     def __repr__(self):
 
@@ -216,11 +135,92 @@ class GameUser(Base):
 #     game_id = Column(Integer(), ForeignKey('games.id'))
 #     user_id = Column(Integer(), ForeignKey('users.id'))
 
-#     game = relationship('Game', back_populates='reviews')
-#     user = relationship('User', back_populates='reviews')
-
 #     def __repr__(self):
 
 #         return f'Review(id={self.id}, ' + \
 #             f'score={self.score}, ' + \
 #             f'game_id={self.game_id})'
+
+# class GameUser(Base):
+#     __tablename__ = "game_users"
+
+#     id = Column(Integer(), primary_key=True)
+#     game_id = Column(ForeignKey('games.id'))
+#     user_id = Column(ForeignKey('users.id'))
+
+#     game = relationship('Game', back_populates='game_users')
+#     user = relationship('User', back_populates='game_users')
+
+#     def __init__(self, game=None, user=None):
+#         self.game = game
+#         self.user = user
+
+#     def __repr__(self):
+
+#         return f'GameUser(game_id={self.game_id}, ' + \
+#             f'user_id={self.user_id})'
+
+#######################################
+
+# Association object model
+# PASSING
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', back_populates='game')
+    users = association_proxy('reviews', 'user',
+        creator=lambda us: Review(user=us))
+
+    def __repr__(self):
+
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', back_populates='user')
+    games = association_proxy('reviews', 'game',
+        creator=lambda gm: Review(game=gm))
+
+    def __repr__(self):
+
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+
+    score = Column(Integer())
+    comment = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
+    game = relationship('Game', back_populates='reviews')
+    user = relationship('User', back_populates='reviews')
+
+    def __repr__(self):
+
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
