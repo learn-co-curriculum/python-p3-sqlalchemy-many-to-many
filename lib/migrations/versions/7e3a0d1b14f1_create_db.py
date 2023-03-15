a72db101e2c000cbc82f18da53c162bd045e27f4
Ben Botsford <ben.botsford@flatironschool.com>
2023-03-15 13:33:17 -0500

    update env.py, models.py

---
+++ lib/migrations/versions/7e3a0d1b14f1_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: 7e3a0d1b14f1
+Revises: 
+Create Date: 2023-03-15 13:31:08.136627
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7e3a0d1b14f1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine, func
-from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, PrimaryKeyConstraint
+from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.associationproxy import association_proxy
 
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///many_to_many.db')
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base()
+Base = declarative_base(metadata=metadata)
 
 # Table object
 # PASSING
